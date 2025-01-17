5cf4832cd0712516585faa816cb93444c108d8dd
alberto-soares <albertosoaress@gmail.com>
2025-01-17 10:40:24 -0300

    Add CI GHA

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Alberto Santos",
+    python_requires=">=3.8",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
